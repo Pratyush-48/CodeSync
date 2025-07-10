c482c6373bd7b49925fd9f48b5753505999d42ee
Pratyush-48 <pratyushprasad16.com>
2025-07-11 02:32:54 +0530

    yes

--- server/index.js
+++ server/index.js
@@ -31,11 +31,6 @@ const languageConfig = {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(_dirname,"/client/build")))
-app.get('*',(_,res)=>{
-  res.sendFile(path.resolve(_dirname,"client","dist","index.html"))
-})
-
 const io = new Server(server, {
   cors: {
     origin: 'https://codesync-hmt6.onrender.com',
@@ -175,3 +170,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.use(express.static(path.join(_dirname,"/client/build")))
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"client","dist","index.html"))
+})
