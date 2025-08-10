84cf4ef314007daaa4212f98944fd5a8daa7e388
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2025-08-10 23:09:25 +0530

    Update Socket.js

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,11 +1,18 @@
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-export const initSocket = async () =>{
+export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempts : 'Infinity',
+        reconnectionAttempts: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL, options);
-}
+
+    const backendURL =
+        process.env.REACT_APP_BACKEND_URL ||
+        (process.env.NODE_ENV === 'production'
+            ? 'https://codesync-hmt6.onrender.com'
+            : 'http://localhost:5000');
+
+    return io(backendURL, options);
+};
