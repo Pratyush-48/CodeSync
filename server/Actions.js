6f70f5e21387af93be1ad48f30336cf4a706b36b
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2025-08-20 17:30:30 +0530

    Update Actions.js

--- server/Actions.js
+++ server/Actions.js
@@ -1,15 +1,14 @@
 // All the events
+
 const ACTIONS = {
-  JOIN: 'join',
-  JOINED: 'joined',
-  DISCONNECTED: 'disconnected',
-  CODE_CHANGE: 'code-change',
-  SYNC_CODE: 'sync-code',
-  SYNC_REQUEST: 'sync-request',
-  LANGUAGE_CHANGE: 'language-change',
-  OUTPUT_CHANGE: 'output-change',
-  LEAVE: 'leave',
-  LEFT: 'left'
+  JOIN: "join",
+  JOINED: "joined",
+  DISCONNECTED: "disconnected",
+  CODE_CHANGE: "code-change",
+  SYNC_CODE: "sync-code",
+  LANGUAGE_CHANGE: "language-change",
+  LEAVE: "leave",
+  
 };
 
-export default ACTIONS;
+module.exports = ACTIONS;
